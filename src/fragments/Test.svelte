56ba274265abf150aa15b43bf93f93b415b30142
full-stack-hacks <redTapeRedTape@protonmail.com>
2025-01-15 13:00:26 -0600

    Added first fragment

---
+++ src/fragments/Test.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    function increment() {
+        console.log('working')
+    }
+
+    const test = () => {
+        console.log('testing')
+    }
+  </script>
+  
+  <button onclick={test} class="btn">
+    h1llo
+  </button>
+
+  <style>
+    .btn {
+        background-color: red;
+        color: black;
+        padding: 1rem;
+        border-radius: 5px;
+    }
+
+    .btn:hover {
+        cursor: pointer;
+    }
+  </style>
